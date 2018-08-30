74be4dfe0b82ba35e3fbad831e166be542cffb56
lgj <469779397@qq.com>
2018-08-30 10:59:54 +0800

    点击歌曲是激活该歌曲

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -9,12 +9,17 @@
       let $el = $(this.el)
       $el.html(this.template)
       let {songs} = data
-      let liList = songs.map((song) => $('<li></li>').text(song.name)
+      let liList = songs.map((song) => $('<li></li>').text(song.name).attr('data-song-id',song.id)
       )
       $el.find('ul').empty()
       liList.map((domLi) => {
         $el.find('ul').append(domLi)
       })
+    },
+    activeItem(li){
+      let $li = $(li)
+      $li.addClass('active')
+      .siblings('.active').removeClass('active')
     },
     clearActive() {
       $(this.el).find('.active').removeClass('active')
@@ -23,19 +28,46 @@
   let model = {
     data: {
       songs: []
+    },
+    find() {let query = new AV.Query('Song')
+      return query.find().then((songs) => {
+
+        this.data.songs = songs.map((song)=>{
+          return{id: song.id,...song.attributes}
+        })
+        return songs
+      })
     }
+
   }
   let controller = {
     init(view, model) {
       this.view = view
       this.model = model
       this.view.render(this.model.data)
+      this.bindEventHbu()
+      this.getAllSongs()
+      this.bindEvent()
+    },
+    bindEvent(){
+      $(this.view.el).on('click','li',(e)=>{
+        this.view.activeItem(e.currentTarget)
+        let songId = e.currentTarget.getAttribute('data-song-id')
+        window.eventHub.emit('select',{id: songId})
+      })
+    },
+    bindEventHbu(){
       window.eventHub.on('upload', () => {
         this.view.clearActive()
       })
       window.eventHub.on('create', (songData) => {
         // songs = ['ADDR 108']
         this.model.data.songs.push(songData)
+        this.view.render(this.model.data)
+      })
+    },
+    getAllSongs(){
+    return  this.model.find().then(()=>{
         this.view.render(this.model.data)
       })
     }
