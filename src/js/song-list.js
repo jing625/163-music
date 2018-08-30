cf09442aa57e49e9f4b9f5115bd78e989c7cbaaa
lgj <469779397@qq.com>
2018-08-30 16:11:02 +0800

    修复update Bug

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -8,18 +8,16 @@
     render(data) {
       let $el = $(this.el)
       $el.html(this.template)
-      let {songs} = data
-      let liList = songs.map((song) => $('<li></li>').text(song.name).attr('data-song-id',song.id)
-      )
+      let {songs,selectedSongId} = data
+      let liList = songs.map((song) => {
+      let $li =  $('<li></li>').text(song.name).attr('data-song-id',song.id)
+      if (song.id === selectedSongId){$li.addClass('active')}
+      return $li
+      })
       $el.find('ul').empty()
       liList.map((domLi) => {
         $el.find('ul').append(domLi)
       })
-    },
-    activeItem(li){
-      let $li = $(li)
-      $li.addClass('active')
-      .siblings('.active').removeClass('active')
     },
     clearActive() {
       $(this.el).find('.active').removeClass('active')
@@ -27,7 +25,8 @@
   }
   let model = {
     data: {
-      songs: []
+      songs: [],
+      selectedSongId: undefined
     },
     find() {let query = new AV.Query('Song')
       return query.find().then((songs) => {
@@ -51,8 +50,10 @@
     },
     bindEvent(){
       $(this.view.el).on('click','li',(e)=>{
-        this.view.activeItem(e.currentTarget)
         let songId = e.currentTarget.getAttribute('data-song-id')
+        this.model.data.selectedSongId = songId
+        this.view.render(this.model.data)
+
         let data
         let songs = this.model.data.songs
         for(let i = 0;i<songs.length;i++){
@@ -73,10 +74,10 @@
       window.eventHub.on('new',()=>{
         this.view.clearActive()
       })
-      window.eventHub.on('update',(song)=>{
+      window.eventHub.on('update', (song)=>{
         let songs = this.model.data.songs
-        for(let i = 0;i < songs.length;i++){
-          if(songs[i].id = song.id){
+        for(let i=0; i<songs.length; i++){
+          if(songs[i].id === song.id){
             Object.assign(songs[i],song)
           }
         }
