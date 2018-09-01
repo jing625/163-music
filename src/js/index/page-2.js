e93351eacaa18ef29de574d9a23da5da56b2d7f4
lgj <469779397@qq.com>
2018-09-01 11:39:28 +0800

    增加热歌榜

--- src/js/index/page-2.js
+++ src/js/index/page-2.js
@@ -1,6 +1,22 @@
 {
   let view = {
     el: '.page-2',
+    template: `
+          <li>
+            <h3>{{song.name}}</h3>
+            <p>
+              <svg class="icon icon-sq">
+                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-sq"></use>
+              </svg>
+              {{song.singer}}
+            </p>
+            <a class="playButton" href="./song.html?id={{song.id}}">
+              <svg class="icon icon-play">
+                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-play"></use>
+              </svg>
+            </a>
+          </li>
+      `,
     init() {
       this.$el = $(this.el)
     },
@@ -9,20 +25,52 @@
     },
     hide() {
       this.$el.removeClass('active')
+    },
+    render(data) {
+      let {songs} = data
+      songs.map((song) => {
+        let $li = $(this.template
+          .replace('{{song.name}}', song.name)
+          .replace('{{song.singer}}', song.singer)
+          .replace('{{song.id}}', song.id)
+        )
+        this.$el.find('ol.page-2-list').append($li)
+        
+      })
     }
   }
-  let model = {}
+  let model = {
+    data: {
+      songs: []
+    },
+    find() {
+      var query = new AV.Query('Song')
+      return query.find().then((songs) => {
+        this.data.songs = songs.map((song) => {
+          return Object.assign({id: song.id}, song.attributes)
+          console.log(3)
+        })
+        return songs
+        console.log(4)
+      })
+    }
+  }
   let controller = {
     init(view, model) {
       this.view = view
       this.view.init()
-      this.model = view
+      this.model = model
       this.bindEventHub()
+      this.model.find().then(() => {
+        console.log(1)
+        this.view.render(this.model.data)
+      })
     },
     bindEventHub() {
       window.eventHub.on('selectTab', (tabName) => {
         if (tabName === 'page-2') {
           this.view.show()
+         
         }else {
           this.view.hide()
         }
